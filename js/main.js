7a7e1e6deb62d70c1d48e4836d67376d37a7d205
Andre Stewart <stewie@localhost.home>
2017-10-19 23:10:00 -0500

    fixes

--- js/main.js
+++ js/main.js
@@ -49,7 +49,7 @@ var soundScore = new buzz.sound("assets/sounds/sfx_point.ogg");
 var soundHit = new buzz.sound("assets/sounds/sfx_hit.ogg");
 var soundDie = new buzz.sound("assets/sounds/sfx_die.ogg");
 var soundSwoosh = new buzz.sound("assets/sounds/sfx_swooshing.ogg");
-buzz.all().setVolume(volume);
+//buzz.all().setVolume(volume);
 
 //loops
 var loopGameloop;
@@ -148,6 +148,7 @@ function startGame()
    
    //jump from the start!
    playerJump();
+   soundScore.play();
 }
 
 function updatePlayer(player)
